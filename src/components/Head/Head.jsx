7cd010cc3fa609de26eeeaa879b567dfee776984
Artyom Smirnov <artyomsmirnov1111@gmail.com>
2024-03-11 12:01:13 +0300

    Created simple header

---
+++ src/components/Head/Head.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./StyledHeader.css";
+
+const Head = () => {
+  return (
+    <div className="styledHeader">
+      <h1>Horoscope for today, yesterday, tomorrow!</h1>
+    </div>
+  );
+};
+
+export default Head;
